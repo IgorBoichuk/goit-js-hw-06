91b7fe873019c78b63e97459a6ed8e2ed7ab95b3
Igor Boichuk <ihor.boichuk@gmail.com>
2023-01-25 17:58:32 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,13 @@
-// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+// Напиши скрипт, який під час набору тексту в інпуті
+// input#name-input (подія input), підставляє його поточне
+// значення в span#name-output. Якщо інпут порожній, у спані
+// повинен відображатися рядок "Anonymous".
 
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+const inputName = document.querySelector("#name-input");
+const nameString = document.querySelector("#name-output");
+
+console.log(inputName);
+console.log(nameString);
