cfcbf326a8fe7496747aeda130a68d8733a29257
Igor <118106419+IgorBoichuk@users.noreply.github.com>
2023-01-26 17:47:56 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+console.log(form);
 
+form.addEventListener("submit", submitOnForm);
+
+function submitOnForm(event) {
+  event.preventDefault();
+
+  console.dir(event.currentTarget.elements);
+
+  const formData = new FormData(event.currentTarget);
+
+  formData.forEach((value, name) => {
+    console.log(value, name);
+  });
+}
+// https://youtu.be/iJq-NwbZL84?t=2268
