7bc66490741ba6f4992ba334a56a05798595bf2f
Igor Boichuk <ihor.boichuk@gmail.com>
2023-01-27 16:40:28 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -22,32 +22,26 @@
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб
 // видаляючи всі створені елементи.
 
-const colorGenerator = document.querySelector("#controls");
+const colorGenerator = document.querySelector("input");
 const createBtn = document.querySelector("button[data-create]");
 const destroyBtn = document.querySelector("button[data-destroy]");
 const divBoxes = document.querySelector("#boxes");
 
-console.log(divBoxes);
-
-const squareDiv = `<div class="div-color">
-<style>
-  .div-color {
-    background-color: red;
-    width: 30px;
-    height: 30px;
-  }
-</style>
-</div>`;
+const squareDiv = `<div class="div-color"></div>`;
 
 createBtn.addEventListener("click", createBoxes);
 destroyBtn.addEventListener("click", destroyBoxes);
 
-// colorGenerator.addEventListener("input", () => {
-//   console.log(colorGenerator.value);
-// });
-
 function createBoxes(amount) {
-  divBoxes.insertAdjacentHTML("afterbegin", squareDiv);
+  if (Number(colorGenerator.value) === 0) {
+    return alert("Задайте кількість блоків для створення!");
+  }
+  console.log(Number(colorGenerator.value));
+
+  // divBoxes.insertAdjacentHTML("afterbegin", squareDiv);
+  // divBoxes.style.backgroundColor = getRandomHexColor();
+  // divBoxes.style.width = "30px";
+  // divBoxes.style.height = "30px";
 }
 
 function destroyBoxes() {
